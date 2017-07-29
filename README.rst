b52c3da8c313cb1609a7bb0c2e2068050700716f
Tim Savage <tim@savage.company>
2017-07-30 01:12:38 +1000

    Initial commit (based off a modified flask version)

---
+++ README.rst
@@ -0,0 +1,5 @@
+===============
+Odin Web Django
+===============
+
+Odin web modules for the `Django <https://www.djangoproject.org>`_ web framework.
---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='odinweb.django',
+    namespace_packages=['odinweb'],
+    version='0.1',
+    description="Toolkit for building web API's using Odin and Django.",
+    long_description=long_description,
+    url='https://github.com/python-odin/odinweb.django',
+    author='Tim Savage',
+    author_email='tim@savage.company',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+
+        'License :: OSI Approved :: BSD License',
+
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+
+    keywords='odin web rest api django',
+
+    packages=find_packages(include=('odinweb',)),
+
+    install_requires=['odinweb', 'django'],
+)
