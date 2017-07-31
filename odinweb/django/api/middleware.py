df75bda8c49ce779505254b7d466ebe22344dbaf
Tim Savage <tim@savage.company>
2017-07-31 12:29:37 +1000

    Added PermissionRequired middleware for django

--- odinweb/django/api/middleware.py
+++ odinweb/django/api/middleware.py
@@ -1,4 +1,5 @@
-from odinweb.exceptions import PermissionDenied
+from odinweb import _compat
+from odinweb.exceptions import PermissionDenied, AccessDenied
 
 
 class LoginRequired(object):
@@ -8,3 +9,18 @@ class LoginRequired(object):
     def pre_dispatch(self, request, _):
         if not request.request.user.is_authenticated:
             raise PermissionDenied(message="Login is required")
+
+
+class PermissionRequired(object):
+    """
+    Certain permission is required for this API.
+    """
+    def __init__(self, perm):
+        if isinstance(perm, _compat.string_types):
+            self.perms = (perm, )
+        else:
+            self.perms = perm
+
+    def pre_dispatch(self, request, _):
+        if not request.request.user.has_perms(self.perms):
+            raise AccessDenied(message="User doesn't have required permissions.")
